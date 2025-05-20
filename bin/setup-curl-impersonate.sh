1ab1102bc61b5fdad08935149d860c7d76474b7d
Christian Scheb <privat@stylemotion.de>
2025-05-20 11:57:34 +0200

    Use full path

--- bin/setup-curl-impersonate.sh
+++ bin/setup-curl-impersonate.sh
@@ -1,6 +1,8 @@
-mkdir ~/curl-impersonate/ || true
-cd ~/curl-impersonate/
+mkdir /home/runner/curl-impersonate/ || true
+cd /home/runner/curl-impersonate/
 
 wget https://github.com/lwthiker/curl-impersonate/releases/download/v0.6.1/libcurl-impersonate-v0.6.1.x86_64-linux-gnu.tar.gz
 tar -xf libcurl-impersonate-v0.6.1.x86_64-linux-gnu.tar.gz
-patchelf --set-soname libcurl.so.4 ~/curl-impersonate/libcurl-impersonate-chrome.so
+patchelf --set-soname libcurl.so.4 /home/runner/curl-impersonate/libcurl-impersonate-chrome.so
+
+ls -l /home/runner/curl-impersonate
