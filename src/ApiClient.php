05b899dd9402d5322572d8014ab664056914b54f
MrNamra <namraramsha@yahoo.com>
2025-05-26 23:14:47 +0530

    401, 419, 429 errors fix (#61)

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Scheb\YahooFinanceApi;
 
+use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Cookie\CookieJar;
+use GuzzleHttp\Cookie\CookieJarInterface;
+use GuzzleHttp\Cookie\SessionCookieJar;
+use Psr\Cache\CacheItemPoolInterface;
 use Scheb\YahooFinanceApi\Exception\ApiException;
 use Scheb\YahooFinanceApi\Results\DividendData;
 use Scheb\YahooFinanceApi\Results\HistoricalData;
@@ -39,18 +43,23 @@ class ApiClient
      */
     private $userAgent;
 
-    public function __construct(ClientInterface $guzzleClient, ResultDecoder $resultDecoder)
+    /**
+     * @var CacheItemPoolInterface
+     */
+    private $cache;
+
+    /**
+     * @var CookieJarInterface
+     */
+    private $cookieJar;
+
+    public function __construct(ClientInterface $guzzleClient, ResultDecoder $resultDecoder, CacheItemPoolInterface $cache)
     {
         $this->client = $guzzleClient;
         $this->resultDecoder = $resultDecoder;
-        $this->userAgent = UserAgent::getRandomUserAgent();
-    }
-
-    public function getHeaders(): array
-    {
-        return [
-            'User-Agent' => $this->userAgent,
-        ];
+        $this->cache = $cache;
+        $this->userAgent = $this->getAgent();
+        $this->cookieJar = $this->getCookies();
     }
 
     /**
@@ -69,9 +78,16 @@ class ApiClient
             .'&region=US&quotesCount='.$limit
             .'&quotesQueryId=tss_match_phrase_query&multiQuoteQueryId=multi_quote_single_token_query&enableCb=false&enableNavLinks=true&enableCulturalAssets=true&enableNews=false&enableResearchReports=false&enableLists=false&listsCount=0&recommendCount=0&enablePrivateCompany=true';
 
-        $responseBody = (string) $this->client->request('GET', $url, ['headers' => $this->getHeaders()])->getBody();
+        $response = $this->client->request('GET', $url, ['headers' => $this->getHeader(), 'cookies' => $this->cookieJar]);
 
-        return $this->resultDecoder->transformSearchResult($responseBody);
+        $status = $response->getStatusCode();
+        if (200 !== $status) {
+            throw new ApiException("Yahoo search failed (HTTP $status)");
+        }
+
+        $body = (string) $response->getBody();
+
+        return $this->resultDecoder->transformSearchResult($body);
     }
 
     /**
@@ -197,26 +213,66 @@ class ApiClient
         return $this->fetchQuotes($currencySymbols);
     }
 
-    private function getCookies(): CookieJar
+    private function getCookies(bool $force = false): CookieJar
     {
-        $cookieJar = new CookieJar();
+        $cacheItem = $this->cache->getItem('yahoo_cookies_tmp');
 
-        // Initialize session cookies
-        $initialUrl = 'https://fc.yahoo.com';
-        $this->client->request('GET', $initialUrl, ['cookies' => $cookieJar, 'http_errors' => false, 'headers' => $this->getHeaders()]);
+        if (!$cacheItem->isHit() || $force) {
+            $jar = new SessionCookieJar('YAHOO_SESSION_TMP', true);
+            $this->client->request('GET', 'https://finance.yahoo.com/quote/AAPL', [
+                'cookies' => $jar,
+                'headers' => [
+                    'User-Agent' => $this->userAgent,
+                    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                    'Accept-Language' => 'en-US,en;q=0.8',
+                    'Referer' => 'https://finance.yahoo.com/',
+                    'Origin' => 'https://finance.yahoo.com',
+                ],
+            ]);
 
-        return $cookieJar;
+            $cacheItem->set(serialize($jar))->expiresAfter(3600);
+            $this->cache->save($cacheItem);
+            $this->cookieJar = $jar;
+        } else {
+            $jar = unserialize($cacheItem->get());
+        }
+
+        return $jar;
     }
 
     /**
      * Get the crumb value from the Yahoo Finance API.
      */
-    private function getCrumb(int $qs, CookieJar $cookies): string
+    private function getCrumb(int $qs = 1, bool $forceRefresh = false): string
     {
-        // Get crumb value
-        $initialUrl = 'https://query'.(string) $qs.'.finance.yahoo.com/v1/test/getcrumb';
+        $cacheItem = $this->cache->getItem('yahoo_crumb');
 
-        return (string) $this->client->request('GET', $initialUrl, ['cookies' => $cookies, 'headers' => $this->getHeaders()])->getBody();
+        if ($forceRefresh || !$cacheItem->isHit()) {
+            $jar = new SessionCookieJar('YAHOO_SESSION_TMP', true);
+
+            $client = new Client([
+                'headers' => [
+                    'User-Agent' => $this->userAgent,
+                    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                ],
+                'cookies' => $this->getCookies(),
+            ]);
+
+            $res = $client->request('GET', 'https://query'.$qs.'.finance.yahoo.com/v1/test/getcrumb');
+            $crumb = (string) $res->getBody();
+
+            // Overwrite the existing cookies with the new ones obtained during the crumb process
+            $cacheItem->set(serialize($jar))->expiresAfter(3600);
+            $this->cache->save($cacheItem);
+            $this->cookieJar = $jar;
+
+            $cacheItem->set($crumb)->expiresAfter(3600);
+            $this->cache->save($cacheItem);
+
+            return $crumb;
+        }
+
+        return $cacheItem->get();
     }
 
     /**
@@ -224,21 +280,33 @@ class ApiClient
      *
      * @return Quote[]
      */
-    private function fetchQuotes(array $symbols)
+    private function fetchQuotes(array $symbols): array
     {
         $qs = $this->getRandomQueryServer();
 
-        // Initialize session cookies
-        $cookieJar = $this->getCookies();
+        $crumb = $this->getCrumb();
 
-        // Get crumb value
-        $crumb = $this->getCrumb($qs, $cookieJar);
+        $url = 'https://query'.$qs.'.finance.yahoo.com/v7/finance/quote?symbols='.urlencode(implode(',', $symbols)).'&crumb='.$crumb;
 
-        // Fetch quotes
-        $url = 'https://query'.$qs.'.finance.yahoo.com/v7/finance/quote?crumb='.$crumb.'&symbols='.urlencode(implode(',', $symbols));
-        $responseBody = (string) $this->client->request('GET', $url, ['cookies' => $cookieJar, 'headers' => $this->getHeaders()])->getBody();
+        for ($retries = 0; $retries <= 1; ++$retries) {
+            if (1 === $retries) {
+                $this->refreshCookiesAndCrumb();
+            }
+            try {
+                $responseBody = (string) $this->client->request('GET', $url, [
+                    'cookies' => $this->cookieJar,
+                    'headers' => $this->getHeader(),
+                ])->getBody();
 
-        return $this->resultDecoder->transformQuotes($responseBody);
+                return $this->resultDecoder->transformQuotes($responseBody);
+            } catch (\Exception $e) {
+                // Retry once, if Still Error
+                if (1 === $retries) {
+                    throw new ApiException("Fail to Fetch data from Yahoo (ERROR {$e->getMessage()})");
+                }
+            }
+        }
+        throw new ApiException('Something Want Wrong!');
     }
 
     private function getHistoricalDataResponseBodyJson(string $symbol, string $interval, \DateTimeInterface $startDate, \DateTimeInterface $endDate, string $filter): string
@@ -246,7 +314,7 @@ class ApiClient
         $qs = $this->getRandomQueryServer();
         $dataUrl = 'https://query'.$qs.'.finance.yahoo.com/v8/finance/chart/'.urlencode($symbol).'?period1='.$startDate->getTimestamp().'&period2='.$endDate->getTimestamp().'&interval='.$interval.'&events='.$filter;
 
-        return (string) $this->client->request('GET', $dataUrl, ['headers' => $this->getHeaders()])->getBody();
+        return (string) $this->client->request('GET', $dataUrl, ['headers' => $this->getHeader()])->getBody();
     }
 
     private function validateIntervals(string $interval): void
@@ -274,15 +342,18 @@ class ApiClient
         $qs = $this->getRandomQueryServer();
 
         // Initialize session cookies
-        $cookieJar = $this->getCookies();
+        $cookieJar = $this->cookieJar;
 
         // Get crumb value
-        $crumb = $this->getCrumb($qs, $cookieJar);
+        $crumb = $this->getCrumb();
 
         // Fetch quotes
         $modules = 'financialData,quoteType,defaultKeyStatistics,assetProfile,summaryDetail';
         $url = 'https://query'.$qs.'.finance.yahoo.com/v10/finance/quoteSummary/'.$symbol.'?crumb='.$crumb.'&modules='.$modules;
-        $responseBody = (string) $this->client->request('GET', $url, ['cookies' => $cookieJar, 'headers' => $this->getHeaders()])->getBody();
+        $responseBody = (string) $this->client->request('GET', $url, [
+            'cookies' => $this->cookieJar,
+            'headers' => $this->getHeader(),
+        ])->getBody();
 
         return $this->resultDecoder->transformQuotesSummary($responseBody);
     }
@@ -291,19 +362,67 @@ class ApiClient
     {
         $qs = $this->getRandomQueryServer();
 
-        // Initialize session cookies
-        $cookieJar = $this->getCookies();
-
         // Get crumb value
-        $crumb = $this->getCrumb($qs, $cookieJar);
+        $crumb = $this->getCrumb($qs);
 
         // Fetch options
         $url = 'https://query'.$qs.'.finance.yahoo.com/v7/finance/options/'.$symbol.'?crumb='.$crumb;
         if ($expiryDate) {
             $url .= '&date='.(string) $expiryDate->getTimestamp();
         }
-        $responseBody = (string) $this->client->request('GET', $url, ['cookies' => $cookieJar, 'headers' => $this->getHeaders()])->getBody();
+        for ($try = 0; $try < 2; ++$try) {
+            try {
+                $responseBody = (string) $this->client->request('GET', $url, ['cookies' => $this->cookieJar])->getBody();
 
-        return $this->resultDecoder->transformOptionChains($responseBody);
+                return $this->resultDecoder->transformOptionChains($responseBody);
+            } catch (\Exception $e) {
+                if (1 === $try) {
+                    throw new ApiException("Fail to Fetch data from Yahoo (ERROR {$e->getMessage()})");
+                }
+                $this->refreshCookiesAndCrumb();
+            }
+        }
+        throw new ApiException('Something Want Wrong!');
+    }
+
+    private function refreshCookiesAndCrumb(): void
+    {
+        $qs = $this->getRandomQueryServer();
+
+        // Fetch crumb using the same cookieJar
+        $this->getAgent(true);
+        $this->getCrumb($qs, true);
+        // hold for 3 seconds for save from quickly calls api
+        sleep(3);
+
+        return;
+    }
+
+    // if all working good without this function we will remove it
+    // private function registerClient(): void
+    // {
+    //     $this->client->request('GET', 'https://finance.yahoo.com/', ['headers' => $this->getHeader()]);
+    // }
+
+    private function getAgent(bool $forceNew = false): string
+    {
+        $cacheItem = $this->cache->getItem('yahoo_user_agent');
+
+        if ($forceNew || !$cacheItem->isHit()) {
+            $agent = UserAgent::getRandomUserAgent();
+            $cacheItem->set($agent)->expiresAfter(3600);
+            $this->cache->save($cacheItem);
+
+            return $agent;
+        }
+
+        return $cacheItem->get();
+    }
+
+    private function getHeader(): array
+    {
+        return [
+            'User-Agent' => $this->userAgent,
+        ];
     }
 }
--- src/ApiClientFactory.php
+++ src/ApiClientFactory.php
@@ -6,14 +6,17 @@ namespace Scheb\YahooFinanceApi;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class ApiClientFactory
 {
-    public static function createApiClient(?ClientInterface $guzzleClient = null): ApiClient
+    public static function createApiClient(?ClientInterface $guzzleClient = null, ?CacheItemPoolInterface $cache = null): ApiClient
     {
         $guzzleClient = $guzzleClient ? $guzzleClient : new Client();
         $resultDecoder = new ResultDecoder(new ValueMapper());
+        $cache = $cache ?? new FilesystemAdapter();
 
-        return new ApiClient($guzzleClient, $resultDecoder);
+        return new ApiClient($guzzleClient, $resultDecoder, $cache);
     }
 }
